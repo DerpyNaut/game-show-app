fa1e0396c31398a0a6849e1dfe35b0f9ee1f701f
Derpy <jukodr@mail.ru>
2019-09-11 21:48:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase ul');
-const missed = 0;
+let missed = 0;
+let letterFound = false;
 
 const startButton = document.querySelector('.btn__reset');
 const startOverlay = document.querySelector('.start');
 const title = document.querySelector('.title');
 const btn = document.querySelectorAll('.keyrow button');
+const tries = document.querySelectorAll('.tries').parentElement;
+
 
 const phrases = ['Frog is rapidly getting angry', 'Coding is fun', 'Playing games is not fun', 'Corgi is derping around', 'Snake eater'];
 
@@ -20,7 +23,7 @@ function getRandomPhraseAsArray (arr) {
   const string = arr[Math.floor(Math.random() * arr.length)].split('');
   console.log(string);
   return string;
-}
+};
 
 function addPhraseToDisplay(arr) {
   for (var i = 0; i<arr.length; i++) {
@@ -32,28 +35,49 @@ function addPhraseToDisplay(arr) {
       li.className = 'letter';
     }
   }
-}
+};
 
 function checkLetter (input) {
   const letters = document.querySelectorAll('.letter');
   const stored = [];
-  console.log(letters);
   for (var i = 0; i < letters.length; i++) {
     if (letters[i].textContent.toLowerCase() === input.toLowerCase()) {
-      letters[i].style.color = 'rgb(0, 0, 0)';
+      letters[i].className += ' show';
       stored.push(letters[i]);
-      console.log('yo');
     }
   }
+  if (stored.length !== 0) {
+    letterFound = true;
+  } else {
+    letterFound = false;
+    missed += 1;
+    tries.removeChild(tries.lastChild);
+  }
+  console.log(stored);
   return stored;
-}
+};
 
+function checkWin() {
+  const letters = document.querySelectorAll('.letter');
+  const shown = document.querySelectorAll('.show');
+  if (letters.length === shown.length) {
+      startOverlay.style.display = '';
+      startOverlay.className += ' win';
+      startOverlay.textContent = 'You won!';
+  } else if (missed >= 5) {
+      startOverlay.style.display = '';
+      startOverlay.className += ' lose';
+      startOverlay.textContent = 'You lost!';
+  }
+};
 
-
-qwerty.addEventListener('keydown', (e) => {
+document.addEventListener('keydown', (e) => {
   for (var i = 0; i < btn.length; i++) {
     if (e.key == btn[i].textContent) {
+      btn[i].className = ' chosen';
+      btn[i].setAttribute('disabled', '');
       checkLetter(e.key);
     }
   }
+  checkWin();
 });
